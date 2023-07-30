03308a178b568eea8acdb23299295c81c7eb7b3d
Henry Merrilees <hmerrilees@hmc.edu>
2023-07-30 03:38:05 -0500

    save

---
+++ README.txt
@@ -0,0 +1,90 @@
+# ToDoom
+The todo list for people with hyper-focus issues. Inspired by _The most dangerous writing app_.
+
+How the world works: recurrent deadlines.
+How I work: one thing at a time until something else screams louder for my attention.
+
+Issues: 
+
+	-- when I'm focused, screams don't really get through.
+
+my brain ---------------------------------------------------------------------------------- the world
+                           ^ gamified todo apps                 ^ todo apps        ^ project management
+
+	-- recurrent deadlines are usually too much to keep straight in my head, but the applications that are supposed to help are generally an order of magnitude more complicated. 
+
+
+Todo apps too much reflect the rigidity of the "real world" to capture how I actually get stuff done, which is by thinking about one thing at a time until something else screams for my attention so loudly that I can't ignore.
+
+
+Daily checklists are both too simple and too demanding. I stop using it once I feel I can remember everything, and exactly then do I forget. When everything is due at the end of the day, I'll work on some other project until it's too late. Hot streaks work a little better, but I can really only hold onto a few at a time.
+
+Then there are todo apps with recurrent tasks, complete with timed calendar integrations.
+
+
+
+Don't plan. Deadlines suck, you will write the thing down, then plan, then replan. Just write it down, and then look at the app you need something to do.
+You only have the time that you have anyway, and only when you have it.
+
+Only once the any particular moment arrives will you have the information to decide what to do with it. A framework of relative "okay so now what am I doing"'s is so much better than at each time I will be doing X. 
+I think a "task stack" cli would actually be super useful.
+
+
+
+
+Scheduling just doesn't work for me. The minute-by-minute calendar is unsurvivable--life gets in the way anyways. Planning takes way too much time, isn't at all flexible, and gives a ridiculously easy failure mechanism. I miss one event and it's over.
+
+Ideologically, I just despise self-scheduling. Spontaneity is in all things beautiful. Forgetfulness is just spontaneity in the negative, no less an exercise of freedom. Without scheduling we would have much less to forget, and that I respect. However, I have zero respect for self-scheduling. "Sorry, I can't [be a normal fun person]," says the self-scheduler, "I have to do this thing that nobody told me I had to and that I don't even want to do." Neither is there spontaneous beauty in forgetting self-orders--you are just back where you started except now also a failure. When I "succeed" in perfectly following my elaborate self-scheduling, it means that I accomplished something so mundane that I had already totally understood it before I even began. 
+
+I'm down with taking time for oneself, be it alone or with others. Scheduling against scheduling itself might be the one thing that scheduling is good for. Negative space is the only room for practice in this world. What I detest is the pretend virtue of forcing oneself to do what one hates. It just drips with self-pity. The same self-pity which perfectly justifies the inevitable failure to follow through.
+
+We treat our poor long-term rational foresight as a deficiency when it does so much to protect us.
+
+
+
+
+
+
+
+Task recurrence features in existing apps (or in those of which I am aware) do not tolerate multiple completions within a given task period.
+
+## design choices
+-- this is to help you make sure that none of your habits slip through the cracks, not to help you remember to do things you would otherwise forget.
+	-- no priority system -- you should just be able to look at your stuff.
+-- period based from last completion.
+	-- to provide stability
+	-- to proivde 
+
+
+-- doesn't need to be dead simple, but complexity should should be dead obvious.
+
+-- people are reactive.
+
+
+## mvp 
+-- "doom screen" progress/health bar...
+
+## upcoming features
+-- hot-streaks, completions, completion logs
+-- complete manifesto
+-- Grace period to tune for schedule drift
+
+## delayed abstractions
+-- better timing implementation
+-- grace period for tasks to preven
+-- daily logging integration (I struggle when my todolist and my notes and my journal are all separate, especially when attempting a daily journal.)
+	-- I really should put this in though
+	-- brainstormed implementation steps:
+		-- <NEW FIELD> Habit {log: Vec<Completion { time: T, log: Option<Text> }>, ..}
+	-- templates?
+	-- Edit and view logs? A log that you can save before you complete the task?
+-- would be a little funny to do live progress bars
+-- One-off tasks 
+-- git vcs
+
+
+## Horrible Ideas
+-- Logs are just zettlekasten notes, building up to a fully featured productivity suite. Bonus points for doing this all without any custom software, all logs being just doubly-linked zettlekasten note list.
+-- Agile/scrum integration
+-- Project management strategies
+
---
+++ src/main.rs
@@ -0,0 +1,239 @@
+#![allow(non_snake_case)]
+use clap::{Parser, Subcommand, ValueEnum};
+use serde::{Deserialize, Serialize};
+use skim::prelude::*;
+use std::collections::btree_map;
+use std::io::Cursor;
+use std::{
+    collections::BTreeMap,
+    env::{temp_dir, var},
+    fs::File,
+    io::{BufReader, BufWriter, Read, Write},
+    path::PathBuf,
+    process::Command,
+    time::{Duration, SystemTime},
+};
+
+use anyhow::{bail, Context, Result};
+
+fn long_edit(content: Option<String>) -> Result<String> {
+    let editor = var("EDITOR").context("EDITOR environment variable not set")?;
+
+    let mut file_path = temp_dir();
+    file_path.push("editable");
+    let file = File::create(&file_path).context("Could not create file")?;
+
+    if let Some(content) = content {
+        BufWriter::new(file)
+            .write_all(content.as_bytes())
+            .context("could not write to tempfile")?;
+    }
+
+    Command::new(editor)
+        .arg(&file_path)
+        .status()
+        .context("could not open editor")?;
+
+    let mut editable = String::new();
+    File::open(file_path)
+        .context("could not open tempfile")?
+        .read_to_string(&mut editable)?;
+    Ok(editable)
+}
+
+#[derive(Serialize, Deserialize)]
+struct Habit {
+    created: SystemTime,
+    logged: SystemTime,
+    period: Duration,
+    name: String,
+    body: String,
+}
+
+// TODO maybe a Completion struct? then a body enum {Habit, Task} that contains Vec<Comepletion> for habit and raw
+// Completion for task. Trying not to prematurely optimize.
+
+impl Habit {
+    fn new(name: String, body: String, period: Duration) -> Self {
+        let created = SystemTime::now();
+        let logged = created.clone();
+
+        Self {
+            created,
+            logged,
+            period,
+            name,
+            body,
+        }
+    }
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: SubCommand,
+}
+
+// TODO if you used a BTreeMap<timebaseduuid, Habit> you could solve a lot of problems
+type Key = String;
+#[derive(Serialize, Deserialize, Default)]
+struct State {
+    todo: BTreeMap<Key, Habit>,
+}
+
+impl State {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn find(&mut self, name: Option<&str>) -> Result<btree_map::OccupiedEntry<Key, Habit>> {
+        let name = name
+            .context("name not provided")
+            .map(String::from)
+            .or_else(|e| {
+                let options = SkimOptionsBuilder::default()
+                    .height(Some("50%"))
+                    .build()
+                    .unwrap();
+
+                // `SkimItemReader` is a helper to turn any `BufRead` into a stream of `SkimItem`
+                // `SkimItem` was implemented for `AsRef<str>` by default
+                let items = SkimItemReader::default().of_bufread(Cursor::new(
+                    self.todo
+                        .iter()
+                        .map(|(k, _v)| format!("{}", k))
+                        .collect::<Vec<_>>()
+                        .join("\n"),
+                ));
+
+                // TODO figure out what these errors acutally are
+                let selected_items = Skim::run_with(&options, Some(items))
+                    .context("Selection error.")?
+                    .selected_items;
+
+                // ensure only one item is selected
+                let item = match selected_items.len() {
+                    0 => bail!("No item selected"),
+                    1 => selected_items[0].text(),
+                    2.. => bail!("Multiple items selected"),
+                    _ => unreachable!(),
+                };
+
+                Ok(item.into())
+            })
+            .context("failure to obtain name")?;
+
+        match self.todo.entry(name) {
+            btree_map::Entry::Vacant(_) => bail!("Item not found."),
+            btree_map::Entry::Occupied(o) => Ok(o),
+        }
+    }
+}
+
+#[derive(Subcommand)]
+enum SubCommand {
+    Add {
+        name: String,
+        #[arg(short, long)]
+        body: Option<String>,
+        period: u64,
+        #[arg(value_enum)]
+        unit: Unit,
+    },
+    Log {
+        name: Option<String>,
+    },
+    Edit {
+        name: Option<String>,
+        // TODO add time adjustment option
+    },
+    List,
+    Remove {
+        name: Option<String>,
+    },
+}
+
+#[derive(ValueEnum, Clone, Serialize, Deserialize)]
+enum Unit {
+    Hours,
+    Days,
+    Weeks,
+    Months,
+    Years,
+}
+
+fn parse_time(duration: u64, unit: Unit) -> Duration {
+    Duration::from_secs(
+        duration
+            * match unit {
+                Unit::Hours => 60 * 60,
+                Unit::Days => 60 * 60 * 24,
+                Unit::Weeks => 60 * 60 * 24 * 7,
+                Unit::Months => 60 * 60 * 24 * 30, //TODO lollll
+                Unit::Years => 60 * 60 * 24 * 365,
+            },
+    )
+}
+
+fn main() -> Result<()> {
+    let home = dirs::home_dir().context("could not find home directory")?;
+    let default_path = home.join(".todoom");
+    dbg!(&default_path);
+    let state_path: PathBuf = var("TODOOM_PATH")
+        .map(PathBuf::from)
+        .unwrap_or(default_path)
+        .into();
+
+    // return file if exists, if open fails, tansform to create new file.
+
+    // TODO, probably a better way to do this with OpenOptions
+    // I did this way because open can error on more than just file non-existence
+    // try_exists returns Ok(False) if confirmed not to exist, we need to handle.
+    let mut state = if state_path.try_exists().is_ok_and(|b| b) {
+        let state_file =
+            File::open(&state_path).context("attempted to open existing path, but found error")?;
+        serde_json::from_reader(BufReader::new(state_file)).context("failed to parse state")?
+    } else {
+        State::new()
+    };
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        SubCommand::Add {
+            name,
+            body,
+            period,
+            unit,
+        } => {
+            let body = body.unwrap_or_else(|| long_edit(None).unwrap());
+            let new = Habit::new(name, body, parse_time(period, unit));
+            state.todo.insert(new.name.clone(), new);
+        }
+        SubCommand::Log { name } => {
+            let mut find = state.find(name.as_deref())?;
+            let habit = find.get_mut();
+            habit.logged = SystemTime::now();
+        }
+        SubCommand::Edit { name } => {
+            let mut find = state.find(name.as_deref())?;
+            let habit = find.get_mut();
+            let body = long_edit(Some(habit.body.clone()))?;
+            habit.body = body;
+        }
+        SubCommand::Remove { name } => {
+            let habit = state.find(name.as_deref())?;
+            habit.remove();
+        }
+        SubCommand::List => {
+            for (name, habit) in state.todo.iter() {
+                println!("{}: {}", name, habit.name);
+            }
+        }
+    }
+
+    let state_file = File::create(state_path).context("failed to create state file")?;
+    serde_json::to_writer_pretty(BufWriter::new(state_file), &state)
+        .context("failed to write state to file")?;
+    Ok(())
+}
