e22c9f574f2c3905b205bf09fb447daf1ab2eeed
Henry Merrilees <hmerrilees@hmc.edu>
2023-08-15 15:26:36 -0500

    grace period

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,49 @@
-#![allow(non_snake_case)]
-use clap::{Parser, Subcommand, ValueEnum};
+use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 use skim::prelude::*;
 use std::collections::btree_map;
 use std::io::Cursor;
 use std::{
     collections::BTreeMap,
-    env::{temp_dir, var},
+    env::var,
     fs::File,
-    io::{BufReader, BufWriter, Read, Write},
+    io::{BufReader, BufWriter},
     path::PathBuf,
-    process::Command,
     time::{Duration, SystemTime},
 };
 
-// TODO get smart about notifications
-
 use anyhow::{bail, Context, Result};
 
-fn long_edit(content: Option<String>) -> Result<String> {
-    let editor = var("EDITOR").context("EDITOR environment variable not set")?;
+mod utils;
+use utils::{long_edit, parse_time, TimeUnit};
 
-    let mut file_path = temp_dir();
-    file_path.push("editable");
-    let file = File::create(&file_path).context("Could not create file")?;
+#[derive(Serialize, Deserialize)]
+struct Config {
+    grace_period: GracePeriod,
+    // TODO, add version so that at least we have a chance of being backwards compatible
+    // potential implementation for new fields: use options on import, and let-else w/
+    // defaults on save.
+}
 
-    if let Some(content) = content {
-        BufWriter::new(file)
-            .write_all(content.as_bytes())
-            .context("could not write to tempfile")?;
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            grace_period: GracePeriod::Fractional(1.1),
+        }
     }
+}
 
-    Command::new(editor)
-        .arg(&file_path)
-        .status()
-        .context("could not open editor")?;
+#[derive(Serialize, Deserialize)]
+enum GracePeriod {
+    Fixed(Duration),
+    Fractional(f64),
+}
 
-    let mut editable = String::new();
-    File::open(file_path)
-        .context("could not open tempfile")?
-        .read_to_string(&mut editable)?;
-    Ok(editable)
+// TODO, solve backwards compatibility issue, see Config
+#[derive(Serialize, Deserialize, Default)]
+struct State {
+    routines: BTreeMap<String, Practice>,
+    config: Config,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -54,20 +57,15 @@ pub struct Practice {
     pub name: String,
     // take notes
     pub body: String,
-    // TODO: make a smart design choice about how to store this
-    //     you're not going to log 3 weeks of a practice
-    // tracked time
     pub cumulative: Duration,
+    // TODO maybe a Completion struct? then a body enum {practice, Task} that contains Vec<Comepletion> for practice and raw
+    // Completion for task. Trying not to prematurely optimize.
 }
-
-// TODO maybe a Completion struct? then a body enum {practice, Task} that contains Vec<Comepletion> for practice and raw
-// Completion for task. Trying not to prematurely optimize.
 
 impl Practice {
     pub fn new(name: String, body: String, period: Duration) -> Self {
         let created = SystemTime::now();
         let logged = created;
-        // TODO heading, or at least heading logic (take first line of body), yeah prob just an impl
 
         Self {
             created,
@@ -79,11 +77,7 @@ impl Practice {
         }
     }
 
-    fn bar(&self, width: usize) -> Result<String> {
-        // TODO, make second bar view that is a timeline i.e. earlist |         █████  ¦                       | latest
-        // maybe                                                      |      ██████████¦███                    | latest
-        //                                                            |          ██████¦███████                | latest
-        let grace_period = 1.1;
+    fn bar(&self, width: usize, grace_period: &GracePeriod) -> Result<String> {
         let now = SystemTime::now();
 
         let elapsed = now.duration_since(self.logged)?;
@@ -93,7 +87,12 @@ impl Practice {
         let end_message = String::new(); // Placeholder in case you want something here
         let bar_width = width - start.len() - end_message.len();
 
-        let filled = ((fraction * bar_width as f64 / grace_period) as usize).min(bar_width);
+        let period = match grace_period {
+            GracePeriod::Fixed(d) => (*d + self.period).as_secs_f64(),
+            GracePeriod::Fractional(f) => self.period.as_secs_f64() * f,
+        };
+
+        let filled = ((fraction * bar_width as f64 / period) as usize).min(bar_width);
 
         let empty = bar_width - filled;
 
@@ -109,38 +108,20 @@ impl Practice {
     }
 }
 
-#[derive(Parser)]
-pub struct Cli {
-    #[command(subcommand)]
-    command: SubCommand,
-}
-
-// TODO if you used a BTreeMap<timebaseduuid, practice> you could solve a lot of problems
-type Key = String;
-#[derive(Serialize, Deserialize, Default)]
-struct State {
-    todo: BTreeMap<Key, Practice>,
-}
-
 impl State {
     fn new() -> Self {
         Self::default()
     }
 
-    fn find(&mut self, name: Option<&str>) -> Result<btree_map::OccupiedEntry<Key, Practice>> {
+    fn find(&mut self, name: Option<&str>) -> Result<btree_map::OccupiedEntry<String, Practice>> {
         let name = name
             .context("name not provided")
             .map(String::from)
             .or_else(|_e| {
-                let options = SkimOptionsBuilder::default()
-                    .height(Some("50%"))
-                    .build()
-                    .unwrap();
+                let options = SkimOptionsBuilder::default().build().unwrap();
 
-                // `SkimItemReader` is a helper to turn any `BufRead` into a stream of `SkimItem`
-                // `SkimItem` was implemented for `AsRef<str>` by default
                 let items = SkimItemReader::default().of_bufread(Cursor::new(
-                    self.todo
+                    self.routines
                         .keys()
                         .map(|k| format!("{}", k))
                         .collect::<Vec<_>>()
@@ -164,11 +145,17 @@ impl State {
             })
             .context("failure to obtain name")?;
 
-        match self.todo.entry(name) {
+        match self.routines.entry(name) {
             btree_map::Entry::Vacant(_) => bail!("Practice not found."),
             btree_map::Entry::Occupied(o) => Ok(o),
         }
     }
+}
+
+#[derive(Parser)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: SubCommand,
 }
 
 #[derive(Subcommand)]
@@ -180,14 +167,14 @@ enum SubCommand {
         body: Option<String>,
         period: u64,
         #[arg(value_enum)]
-        unit: Unit,
+        unit: TimeUnit,
     },
     /// Track a practice, specify name or select from list.
     Track {
         name: Option<String>,
         time: u64,
         #[arg(value_enum)]
-        unit: Unit,
+        unit: TimeUnit,
     },
     /// Edit log, specify name or select from list.
     Log {
@@ -198,7 +185,7 @@ enum SubCommand {
         name: Option<String>,
         period: Option<u64>,
         #[arg(value_enum)]
-        unit: Option<Unit>,
+        unit: Option<TimeUnit>,
     },
     /// View list of practices.
     List,
@@ -215,31 +202,9 @@ enum SubCommand {
     Reset,
 }
 
-#[derive(ValueEnum, Clone, Serialize, Deserialize)]
-enum Unit {
-    Hours,
-    Days,
-    Weeks,
-    Months,
-    Years,
-}
-
-fn parse_time(duration: u64, unit: Unit) -> Duration {
-    Duration::from_secs(
-        duration
-            * match unit {
-                Unit::Hours => 60 * 60,
-                Unit::Days => 60 * 60 * 24,
-                Unit::Weeks => 60 * 60 * 24 * 7,
-                Unit::Months => 60 * 60 * 24 * 30, //TODO lollll
-                Unit::Years => 60 * 60 * 24 * 365,
-            },
-    )
-}
-
 fn main() -> Result<()> {
     let home = dirs::home_dir().context("could not find home directory")?;
-    let default_path = home.join(".practice"); // TODO to userdata home
+    let default_path = home.join(".practice"); // TODO to userdata home for easy git saving
     let state_path: PathBuf = var("PRACTICE_PATH")
         .map(PathBuf::from)
         .unwrap_or(default_path);
@@ -268,7 +233,7 @@ fn main() -> Result<()> {
         } => {
             let body = body.unwrap_or_else(|| long_edit(None).unwrap());
             let new = Practice::new(name, body, parse_time(period, unit));
-            state.todo.insert(new.name.clone(), new);
+            state.routines.insert(new.name.clone(), new);
         }
         SubCommand::Track { name, time, unit } => {
             let mut find = state.find(name.as_deref())?;
@@ -289,33 +254,42 @@ fn main() -> Result<()> {
             practice.remove();
         }
         SubCommand::List => {
-            let max_name_len = state.todo.keys().map(|name| name.len()).max().unwrap_or(0);
+            let max_name_len = state
+                .routines
+                .keys()
+                .map(|name| name.len())
+                .max()
+                .unwrap_or(0);
             let max_name_len = max_name_len.max(30);
             let term_width = termsize::get().context("failed to obtain termsize")?.cols;
             let bar_width = term_width as usize - max_name_len - 1;
 
             println!();
-            for (name, practice) in state.todo.iter() {
+            for (name, practice) in state.routines.iter() {
                 let mut n = name.clone(); // There has to be a better way?
                 n.truncate(max_name_len);
-                println!(" {:>max_name_len$}{}", name, practice.bar(bar_width)?);
+                println!(
+                    " {:>max_name_len$}{}",
+                    name,
+                    practice.bar(bar_width, &state.config.grace_period)?
+                );
             }
             println!();
         }
         SubCommand::Rename { name, new_name } => {
-            let mut practice = state.todo.remove(&name).context("practice not found")?;
+            let mut practice = state.routines.remove(&name).context("practice not found")?;
             practice.name = new_name.clone();
-            state.todo.insert(new_name, practice);
+            state.routines.insert(new_name, practice);
         }
         SubCommand::Reset => {
-            for practice in state.todo.values_mut() {
+            for practice in state.routines.values_mut() {
                 practice.logged = SystemTime::now();
             }
         }
         SubCommand::EditPeriod { name, period, unit } => {
             let mut find = state.find(name.as_deref())?;
             let practice = find.get_mut();
-            let period = parse_time(period.unwrap_or(0), unit.unwrap_or(Unit::Hours));
+            let period = parse_time(period.unwrap_or(0), unit.unwrap_or(TimeUnit::Hours));
             practice.period = period;
         }
     }
